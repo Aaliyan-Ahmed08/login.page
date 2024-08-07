6ecf0918ad3204b2337ccd5d7837921910690dba
Your Name <you@example.com>
2024-08-07 23:29:15 +0500

    initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+// console.log('Script loaded');
+// document.getElementById('loginForm').addEventListener('submit', function(event) {
+//     event.preventDefault(); // Prevent the form from submitting normally
+//     console.log('Form submission prevented, redirecting...');
+//     window.location.href = 'welcome.html'; // Redirect to another page
+// });
